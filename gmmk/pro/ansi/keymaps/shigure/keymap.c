5e0264cbe7c689d9507ff11c9dad9935aad5d983
Shigure <frozen0416@gmail.com>
2021-07-27 23:48:51 -0400

    Update Readme, add git ignore, setup one shot mods

--- gmmk/pro/ansi/keymaps/shigure/keymap.c
+++ gmmk/pro/ansi/keymaps/shigure/keymap.c
@@ -1,6 +1,6 @@
 /* Copyright 2021 Shigure<frozen0416@gmail.com> 
 
-ver. 07/26/2021
+ver. 07/27/2021
 Some functions modified based on jonavin's keymap
 
 This program is free software: you can redistribute it and/or modify
@@ -98,6 +98,9 @@ void ql_reset(qk_tap_dance_state_t *state, void *user_data);
 
 #define CAP_LYR TD(CAPS_LAYR)
 #define TSK_MGR C(S(KC_ESC))
+#define OSK_ALT OSM(MOD_LALT)
+#define OSK_SFT OSM(MOD_RSFT)
+#define OSK_CTL OSM(MOD_LCTL)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -121,8 +124,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSDEL,         KC_HOME,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
         CAP_LYR, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGDN,
-        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_END,
-        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, TT(2),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          OSK_SFT, KC_UP,   KC_END,
+        OSK_CTL, KC_LGUI, OSK_ALT,                            KC_SPC,                             KC_RALT, TT(2),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 	
     [1] = LAYOUT(
