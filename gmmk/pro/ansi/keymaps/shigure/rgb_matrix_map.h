536deb69f85d724567170eb9ac92fe90dd69a8fc
Shigure <frozen0416@gmail.com>
2021-08-01 13:22:49 -0400

    update layer1 backlight

--- gmmk/pro/ansi/keymaps/shigure/rgb_matrix_map.h
+++ gmmk/pro/ansi/keymaps/shigure/rgb_matrix_map.h
@@ -120,23 +120,34 @@ enum led_location_map
 };
 
 const uint8_t LED_LAYER1[] = {LED_F1, LED_F4, LED_F5, LED_F6, LED_F7, LED_F8, LED_F12, LED_INS,
-                              LED_1, LED_7, LED_8, LED_9,
-                              LED_Q, LED_W, LED_E, LED_R, LED_U, LED_I, LED_O,
-                              LED_A, LED_S, LED_D, LED_F, LED_J, LED_K, LED_L,
-                              LED_M, LED_COMM, LED_DOT,
-                              LED_MINS, LED_EQL, LED_LBRC, LED_RBRC,
-                              LED_FN,
-                              LED_L1, LED_L2, LED_L3, LED_L4, LED_L5, LED_L6, LED_L7, LED_L8, LED_R1, LED_R2, LED_R3, LED_R4, LED_R5, LED_R6, LED_R7, LED_R8};
+                              LED_1, //LED_7, LED_8, LED_9,
+                              LED_Q, /*LED_W,*/ LED_E, LED_R, //LED_U, LED_I, LED_O,
+                              /*LED_A, LED_S, LED_D,*/ LED_F, //LED_J, LED_K, LED_L,
+                              //LED_M, LED_COMM, LED_DOT,
+                              //LED_MINS, LED_EQL, LED_LBRC, LED_RBRC,
+                            LED_FN,
+                            LED_L1, LED_L2, LED_L3, LED_L4, LED_L5, LED_L6, LED_L7, LED_L8, LED_R1, LED_R2, LED_R3, LED_R4, LED_R5, LED_R6, LED_R7, LED_R8};
 
 const uint8_t LED_LAYER2[] = {LED_F1, LED_F2, LED_F3, LED_INS,
-                              LED_2,
-                              LED_C, LED_M, LED_FN,
-                              LED_LEFT, LED_RIGHT, LED_UP, LED_DOWN,
-                              LED_PGUP, LED_PGDN, LED_END, LED_DEL,
-                              LED_L1, LED_L2, LED_L3, LED_L4, LED_L5, LED_L6, LED_L7, LED_L8, LED_R1, LED_R2, LED_R3, LED_R4, LED_R5, LED_R6, LED_R7, LED_R8};
+                            LED_2,
+                            LED_C, LED_M, LED_FN,
+                            LED_LEFT, LED_RIGHT, LED_UP, LED_DOWN,
+                            LED_PGUP, LED_PGDN, LED_END, LED_DEL,
+                            LED_L1, LED_L2, LED_L3, LED_L4, LED_L5, LED_L6, LED_L7, LED_L8, LED_R1, LED_R2, LED_R3, LED_R4, LED_R5, LED_R6, LED_R7, LED_R8};
 
 const uint8_t LED_LAYER3[] = {LED_3, LED_FN};
 
 const uint8_t LED_SIDE[] = {LED_L1, LED_L2, LED_L3, LED_L4, LED_L5, LED_L6, LED_L7, LED_L8, LED_R1, LED_R2, LED_R3, LED_R4, LED_R5, LED_R6, LED_R7, LED_R8};
+
+const uint8_t LED_NUMPAD[] = {
+        LED_7, LED_8, LED_9,
+        LED_U, LED_I, LED_O,
+        LED_J, LED_K, LED_L,
+        LED_M, LED_COMM, LED_DOT,
+		LED_MINS, LED_EQL, LED_LBRC, LED_RBRC
+        };
+
+const uint8_t LED_WASD[] = {
+    LED_W, LED_A, LED_S, LED_D};
 
 #endif
